2276b705a279fc09423225311f559f57e28bcd9e
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-16 23:21:20 +0700

    Fix hover <li>
    Fix hover <li>

--- JS/mp4List.js
+++ JS/mp4List.js
@@ -1,11 +1,23 @@
-function loadPlaylistVideo() {
+function loadFirstVideo(){
+    var listElementLI = document.getElementsByTagName("li");
+    for (var i = 0; i < listElementLI.length; i++) {
+        if (listElementLI[i].parentNode.id === "playlist-video") {
+            listElementLI[i].setAttribute("style", "backgroud-color: cyan;");
+            var urlVS = listElementLI[i].getAttribute("urlVideoSource");
+            document.getElementById("video-area").setAttribute("src", urlVS);
+            document.getElementById("video-area").setAttribute("autoplay", "true");
+            break;
+        }
+    }
+}
+function loadOnclick() {
     /**
      * Set default color for all element <li>
      */
     var listElementLI = document.getElementsByTagName("li");
     for (var i = 0; i < listElementLI.length; i++) {
         if (listElementLI[i].parentNode.id === "playlist-video") {
-            listElementLI[i].setAttribute("style", "backgroud-color: white;");
+            listElementLI[i].setAttribute("style", "color: white;");
         }
     }
     /**
