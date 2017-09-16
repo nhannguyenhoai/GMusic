49f2cbd754025c0c0a373aaf0fda51bedf806edd
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-16 22:39:32 +0700

    Fix hover <li>
    Fix hover <li>

--- JS/mp4List.js
+++ JS/mp4List.js
@@ -5,7 +5,7 @@ function loadPlaylistVideo() {
     var listElementLI = document.getElementsByTagName("li");
     for (var i = 0; i < listElementLI.length; i++) {
         if (listElementLI[i].parentNode.id === "playlist_video") {
-            listElementLI[i].setAttribute("style", "color:#0BF521;");
+            listElementLI[i].setAttribute("style", "color: #0BF521;");
         }
     }
     /**
