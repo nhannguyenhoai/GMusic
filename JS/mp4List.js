967591a3a22fd1a4bf5321d9579c6ea57d6e50e1
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-16 22:14:22 +0700

    Fix playlist video
    Fix JS for playlist video

--- JS/mp4List.js
+++ JS/mp4List.js
@@ -1,6 +1,21 @@
 function loadPlaylistVideo() {
+    /**
+     * Set default color for all element <li>
+     */
+    var listElementLI = document.getElementsByTagName("li");
+    for (var i = 0; i < listElementLI.length; i++) {
+        if (listElementLI[i].parentNode.id === "playlist_video") {
+            listElementLI[i].setAttribute("style", "color:#0BF521");
+        }
+    }
+    /**
+     * Get attribute of element <li> clicked, then change element <video> source
+     */
     var urlVS = window.event.target.getAttribute("urlVideoSource");
-    document.getElementById("video_area").setAttribute("src",urlVS);
+    document.getElementById("video_area").setAttribute("src", urlVS);
     document.getElementById("video_area").setAttribute("autoplay", "true");
-    window.event.target.setAttribute("style", "color: purple;");
+    /**
+     * Return set attribute style color of element <li> clicked
+     */
+    return window.event.target.setAttribute("style", "color: blue;");
 }
