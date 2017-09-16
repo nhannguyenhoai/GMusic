986e99867569cdb7db775619b515eef4fbcda30c
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-17 02:46:49 +0700

    Fix playlist auto load next

--- JS/mp4List.js
+++ JS/mp4List.js
@@ -19,18 +19,20 @@ function loadFirst(mediaId, playlistId, statusId) {
     var childs = document.getElementsByTagName("li");
     var countChilds = childs.length;
     var currentChilds = 0;
-    for (var i = 0; i < countChilds; i++) {
-        if (childs[i].parentNode.id === playlistId) {
-            var childurlSrc = childs[i].getAttribute("urlSrc");
-            document.getElementById(mediaId).setAttribute("src", childurlSrc);
-            document.getElementById(mediaId).setAttribute("autoplay", "true");
-            document.getElementById(statusId).innerHTML = ("Video playing: " + childurlSrc).replace("Public/mp4/", "");
-            currentChilds = i;
-            currentChilds++;
-            break;
+    if (currentChilds < countChilds) {
+        for (var i = 0; i < countChilds; i++) {
+            if (childs[i].parentNode.id === playlistId) {
+                var childurlSrc = childs[i].getAttribute("urlSrc");
+                document.getElementById(mediaId).setAttribute("src", childurlSrc);
+                document.getElementById(mediaId).setAttribute("autoplay", "true");
+                document.getElementById(statusId).innerHTML = ("Video playing: " + childurlSrc).replace("Public/mp4/", "");
+                currentChilds = i;
+                currentChilds++;
+                break;
+            }
         }
+        loadNext(mediaId, playlistId, statusId, countChilds, childs, currentChilds);
     }
-    loadNext(mediaId, playlistId, statusId, countChilds, childs, currentChilds);
 }
 function loadOnclick(mediaId, playlistId, statusId) {
     var childs = document.getElementsByTagName("li");
@@ -40,15 +42,17 @@ function loadOnclick(mediaId, playlistId, statusId) {
     document.getElementById(mediaId).setAttribute("src", urlSrc);
     document.getElementById(mediaId).setAttribute("autoplay", "true");
     document.getElementById(statusId).innerHTML = ("Video playing: " + urlSrc).replace("Public/mp4/", "");
-    for (var i = 0; i < countChilds; i++) {
-        if(window.event.target === childs[i]){
-            currentChilds = i;
-            currentChilds++;
-            break;
+    if (currentChilds < countChilds) {
+        for (var i = 0; i < countChilds; i++) {
+            if (window.event.target === childs[i]) {
+                currentChilds = i;
+                currentChilds++;
+                break;
+            }
+            else {
+                currentChilds++;
+            }
         }
-        else{
-            currentChilds++;
-        }
+        loadNext(mediaId, playlistId, statusId, countChilds, childs, currentChilds);
     }
-    loadNext(mediaId, playlistId, statusId, countChilds, childs, currentChilds);
 }
