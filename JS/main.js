279a2e2ab79ec3c5b5bb5f91a5ab92849f774cbe
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-19 23:41:54 +0700

    Change GUI

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,11 @@
-function getAllChildByParentClassname(childTag, parentClassname) {
-    var childsByTag = document.getElementsByTagName(childTag);
-    var childsByParent = [];
-    var index = 0;
-    for (var i = 0; i < childsByTag.length; i++) {
-        if (childsByTag[i]. parentNode.className  === parentClassname) {
-            childsByParent[index] = childsByTag[i];
-            index++;
-        }
-    }
-    return childsByParent;
+function openNav() {
+    document.getElementById("nav-bar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(12, 14, 19, 1)";
 }
 
-function setDefaultColor(childs, color) {
-    for (var i = 0; i < childs.length; i++) {
-        childs[i].setAttribute("style", "color: " + color);
-    }
-}
-
-function changeNavColor(navListLiId, defaultColor, color) {
-    var navOnClick = window.event.target;
-    var navChilds = getAllChildByParentClassname('a', navListLiId);
-    setDefaultColor(navChilds, defaultColor);
-    for (var i = 0; i < navChilds.length; i++) {
-        if (navChilds[i] === navOnClick) {
-            navChilds[i].setAttribute("style", "color: " + color);
-        }
-    }
+function closeNav() {
+    document.getElementById("nav-bar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "rgba(12, 14, 19, 1)";
 }
