9799e6fa3ebebf35a8ad3270cc6f5625210ac81b
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-20 00:05:27 +0700

    Update GUI

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,11 @@
 function openNav() {
     document.getElementById("nav-bar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main-content").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(12, 14, 19, 1)";
 }
 
 function closeNav() {
     document.getElementById("nav-bar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main-content").style.marginLeft = "0";
     document.body.style.backgroundColor = "rgba(12, 14, 19, 1)";
 }
