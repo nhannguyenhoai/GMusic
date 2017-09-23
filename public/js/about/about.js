936d849e0b1b37b3703a3daae7b8cbd9b430b3fb
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-24 01:52:59 +0700

    WEB GUI 1.4

--- public/js/about/about.js
+++ public/js/about/about.js
@@ -1,3 +1,3 @@
 function setAbout() {
-    document.getElementById("main").innerHTML = "<center><img src='public/image/logo.png' style='border-radius: 50%;' width=30% heigh=30%></img><br><br><p style='color: cyan; font-size:22px; font-weight: bold'>This website is designed &copy; 2017 by Nhan, version 1.0</p><br><p style='color: cyan; font-size: 22px; font-weight: bold;'>Only using this website for non-econemic purpose</p></center>";
+    document.getElementById("main").innerHTML = "<center><img src='public/image/logo.png' style='border-radius: 50%;' width=30% heigh=30%></img><br><br><p style='color: red; font-weight: bold'>This website is designed &copy; 2017 by Nhan, version 1.0</p><br><p style='color: red; font-weight: bold;'>Only using this website for non-econemic purpose</p></center>";
 }
--- public/js/nav/nav.js
+++ public/js/nav/nav.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("nav").style.width = "100px";
+    document.getElementById("nav").style.width = "20%";
 }
 function closeNav() {
     document.getElementById("nav").style.width = "0";
