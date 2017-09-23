575161c53b7a74fb8a2884f7468abb78f7854201
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-23 14:27:19 +0700

    Web GUI version 1.1

--- public/js/nav/nav.js
+++ public/js/nav/nav.js
@@ -1,8 +1,6 @@
 function openNav() {
     document.getElementById("nav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
 }
 function closeNav() {
     document.getElementById("nav").style.width = "0";
-    document.body.style.backgroundColor = "white";
 }
