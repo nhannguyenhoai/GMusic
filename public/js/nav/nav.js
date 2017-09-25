0700e4c43b6cc710640a83b1e7135fb05258f716
Nhan Nguyen Hoai <acc.reg.goog@gmail.com>
2017-09-25 21:53:52 +0700

    Update WEB GUI version 1.0

--- public/js/nav/nav.js
+++ public/js/nav/nav.js
@@ -1,6 +1,12 @@
 function openNav() {
     document.getElementById("nav").style.width = "200px";
+    window.setTimeout(function () {
+        document.getElementById("nav__close-icon").innerHTML = "&#8672;";
+    }, 500);
 }
 function closeNav() {
     document.getElementById("nav").style.width = "0";
+    window.setTimeout(function () {
+        document.getElementById("nav__close-icon").innerHTML = "&#8674;";
+    }, 1000);
 }
